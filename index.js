d54e4653ef95c06c66977e7cbb8ffdbc47c342c3
Marcem55 <malacalzamarcelo@gmail.com>
2024-07-24 17:04:13 -0300

    [ADD] exercises 3.1 to 3.6 completed

---
+++ index.js
@@ -0,0 +1,95 @@
+const express = require("express");
+const app = express();
+
+app.use(express.json());
+
+let persons = [
+  {
+    id: 1,
+    name: "Arto Hellas",
+    number: "040-123456",
+  },
+  {
+    id: 2,
+    name: "Ada Lovelace",
+    number: "39-44-5323523",
+  },
+  {
+    id: 3,
+    name: "Dan Abramov",
+    number: "12-43-234345",
+  },
+  {
+    id: 4,
+    name: "Mary Poppendieck",
+    number: "39-23-6423122",
+  },
+];
+
+const generateId = () => {
+  return Math.floor(Math.random() * 1000000000);
+};
+
+app.post("/api/persons", (req, res) => {
+  const body = req.body;
+  if (!body.name || !body.number) {
+    return res.status(400).json({
+      error: "Missing parameters, please fill all the fields",
+    });
+  }
+  const existingName = persons.find(
+    (p) => p.name.toLowerCase() === body.name.toLowerCase()
+  );
+  if (existingName) {
+    return res.status(400).json({
+      error: `${body.name} is already added to phonebook. Name must be unique`,
+    });
+  }
+  const person = {
+    id: generateId(),
+    name: body.name,
+    number: body.number,
+  };
+  persons = persons.concat(person);
+  res.json(person);
+});
+
+app.get("/api/persons", (req, res) => {
+  res.json(persons);
+});
+
+app.get("/info", (req, res) => {
+  const infoText = `<p>Phonebook has info for ${persons.length} people</p>`;
+  const requestedAt = `<p>${new Date(Date.now()).toString()}</p>`;
+
+  res.send(`${infoText} ${requestedAt}`);
+});
+
+app.get("/api/persons/:id", (req, res) => {
+  const id = req.params.id;
+
+  const person = persons.find((p) => p.id === Number(id));
+  if (!person) {
+    return res.status(400).json({
+      error: `No person with id ${id} founded`,
+    });
+  }
+  res.json(person);
+});
+
+app.delete("/api/persons/:id", (req, res) => {
+  const id = req.params.id;
+  const person = persons.find((p) => p.id === Number(id));
+  if (!person) {
+    return res.status(400).json({
+      error: `No person with id ${id} founded`,
+    });
+  }
+  persons = persons.filter((p) => p.id !== Number(id));
+  res.status(204).send();
+});
+
+const PORT = 3001;
+app.listen(PORT, () => {
+  console.log(`Server running in PORT ${PORT}`);
+});
